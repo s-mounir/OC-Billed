9bbad9eec00ec8c85a4ef82e121feb5a63f8e417
Sara Mounir <sara_mounir@hotmail.fr>
2023-07-20 15:54:49 +0200

    test views bills

--- bill-app/Billed-app-FR-Front/src/__tests__/Bills.js
+++ bill-app/Billed-app-FR-Front/src/__tests__/Bills.js
@@ -2,6 +2,8 @@
  * @jest-environment jsdom
  */
 
+import '@testing-library/jest-dom'
+
 import {screen, waitFor} from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
@@ -26,7 +28,7 @@ describe("Given I am connected as an employee", () => {
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
       //to-do write expect expression
-
+      expect(windowIcon).toHaveClass('active-icon');
     })
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
